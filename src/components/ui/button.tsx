49835384374248fa3db04db4a3448fcc8d585499
MatheusPalmieri <matheuspalmieridev@gmail.com>
2024-07-25 01:23:50 -0300

    feat: loading button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,5 +1,6 @@
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
+import { LoaderCircle } from 'lucide-react';
 import * as React from 'react';
 
 import { cn } from '@/lib/utils';
@@ -33,21 +34,38 @@ export interface ButtonProps
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
   isSubmit?: boolean;
+  isLoading?: boolean;
+  isDisabled?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (
-    { className, variant, size, asChild = false, type = 'button', isSubmit = false, ...props },
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      type = 'button',
+      isSubmit = false,
+      isLoading,
+      isDisabled,
+      ...props
+    },
     ref,
   ) => {
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         type={isSubmit ? 'submit' : type}
+        className={cn(buttonVariants({ variant, size, className }))}
+        disabled={isDisabled || isLoading}
         {...props}
-      />
+      >
+        {isLoading && <LoaderCircle className='mr-2 h-4 w-4 animate-spin' />}
+
+        {props?.children}
+      </Comp>
     );
   },
 );
