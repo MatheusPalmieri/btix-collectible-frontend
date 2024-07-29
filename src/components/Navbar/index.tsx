8a013a28e47791423fe410f3bb2cf41603774f71
MatheusPalmieri <matheuspalmieridev@gmail.com>
2024-07-29 12:05:57 -0300

    feat: logout

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,15 @@
 import Logo from '@/assets/images/logo/logo.png';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useAuthStore } from '@/contexts/auth';
 import { ChevronLeft } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 import { Button } from '../ui/button';
 
 interface Props {
@@ -8,8 +17,23 @@ interface Props {
 }
 
 export const Navbar = ({ back }: Props) => {
+  const { user, signOut } = useAuthStore();
   const navigate = useNavigate();
+
   const handleBack = () => back && navigate(`/${back === '/' ? '' : back}`);
+  const handleRedirect = (path: string) => navigate(`/${path}`);
+
+  const fallback = user?.id
+    ? user?.name
+        .split(' ')
+        .map((name) => name[0])
+        .join('')
+    : '';
+
+  const handleSignOut = () => {
+    signOut();
+    navigate('/sign-in');
+  };
 
   return (
     <header className='w-full border-b border-secondary-300/80 shadow-xl shadow-secondary-500/20'>
@@ -32,7 +56,27 @@ export const Navbar = ({ back }: Props) => {
           <img src={Logo} alt='Logo Btix' className='h-10 mx-auto' />
         </div>
 
-        <div className='w-[25%]'></div>
+        <div className='w-[25%] flex items-center justify-end'>
+          {user?.id && (
+            <DropdownMenu>
+              <DropdownMenuTrigger className='bg-secondary-100/50 flex items-center gap-2 pl-2 rounded-l-lg rounded-r-xl outline-none'>
+                <p className='text-sm font-bold'>{user?.name?.split(' ')[0]}</p>
+                <Avatar className='size-6'>
+                  <AvatarImage src={user?.avatar} />
+                  <AvatarFallback>{fallback}</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align='end'>
+                <DropdownMenuItem onClick={() => handleRedirect('')}>Home</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => handleRedirect('profile')}>
+                  Profile
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={handleSignOut}>Logout</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </div>
       </div>
     </header>
   );
