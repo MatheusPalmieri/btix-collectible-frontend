49956a45a55ddab82c7269d3296e66cf54a8cd21
MatheusPalmieri <matheuspalmieridev@gmail.com>
2024-08-12 16:49:17 -0300

    feat: page access

--- src/pages/Private/Passport/index.tsx
+++ src/pages/Private/Passport/index.tsx
@@ -1,11 +1,38 @@
-import { Access } from '@/components/Access';
+import Logo from '@/assets/images/logo/icon-white.png';
 import { Container } from '@/components/Container';
+import { Button } from '@/components/ui/button';
+import { useAuthStore } from '@/contexts/auth';
+import QRCode from 'qrcode.react';
 
 export const PagePassport = () => {
+  const { user } = useAuthStore();
+
   return (
     <Container back='/' type='medium' className='flex flex-col'>
+      <div className='text-center space-y-1'>
+        <h2 className='text-lg font-semibold leading-none tracking-tight'>{user?.name}</h2>
+
+        <p className='text-sm text-muted-foreground'>Seu acesso pessoal exclusivo</p>
+      </div>
+
+      <div className='bg-secondary-200 rounded-lg p-4 mx-auto'>
+        <QRCode
+          value='access_personal_token'
+          bgColor='#4c4c4c'
+          fgColor='#fff'
+          size={300}
+          imageSettings={{
+            src: Logo,
+            width: 50,
+            height: 50,
+            excavate: true,
+          }}
+          style={{ borderRadius: 4 }}
+        />
+      </div>
+
       <section className='flex-1 flex items-end justify-end'>
-        <Access full variant='default' />
+        <Button className='w-full md:w-96 font-semibold'>Save</Button>
       </section>
     </Container>
   );
