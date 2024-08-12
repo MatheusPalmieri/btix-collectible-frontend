0433539d001e48909b9c914785ec97ead218ee1e
MatheusPalmieri <matheuspalmieridev@gmail.com>
2024-08-12 18:34:34 -0300

    feat: club

--- src/pages/Public/Club/index.tsx
+++ src/pages/Public/Club/index.tsx
@@ -1,9 +1,12 @@
 import { Container } from '@/components/Container';
+import { useParams } from 'react-router-dom';
 
 export const PageClub = () => {
+  const { slug } = useParams();
+
   return (
     <Container back='/' type='medium'>
-      <h1>Club</h1>
+      <h1>{slug}</h1>
     </Container>
   );
 };
